bd2a04f5f5c572a51636817b6ba7949f4f9cea64
Rae-An Andres <raeandres@users.noreply.github.com>
2026-03-04 21:53:55 +0800

    Added tool calling; updated model to use local Ollama model
    Used local model
    Added Tool calling

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,68 @@
 package org.example
 
+import ai.koog.agents.core.agent.AIAgent
+import ai.koog.agents.core.tools.ToolRegistry
+import ai.koog.agents.core.tools.annotations.LLMDescription
+import ai.koog.agents.core.tools.annotations.Tool
+import ai.koog.agents.core.tools.reflect.tool
 import ai.koog.prompt.dsl.prompt
 import ai.koog.prompt.executor.clients.LLMClient
 import ai.koog.prompt.executor.clients.google.GoogleLLMClient
 import ai.koog.prompt.executor.clients.google.GoogleModels
+import ai.koog.prompt.executor.llms.SingleLLMPromptExecutor
+import ai.koog.prompt.executor.llms.all.simpleGoogleAIExecutor
+import ai.koog.prompt.executor.llms.all.simpleOllamaAIExecutor
 
 import ai.koog.prompt.llm.LLModel
+import ai.koog.prompt.llm.OllamaModels
 import ai.koog.prompt.params.LLMParams
+
+
+@Tool
+@LLMDescription("Sends money to specified recipient with given amount and purpose")
+fun sendMoney(
+    @LLMDescription("Amount of money to send in Pesos")
+    amount: Double,
+    @LLMDescription("Name of the recipient")
+    recipient:String,
+    @LLMDescription("Purpose of the Money Transfer")
+    purpose:String) : String {
+    println("----------------------------------------------")
+    println("Sending money to $recipient for $amount pesos with purpose: $purpose")
+    println("Please confirm the transaction by typing 'yes'")
+    println("----------------------------------------------")
+    val confirmation = readln()
+
+    return if (confirmation.lowercase() == "yes") "Money Sent" else "Transaction declined"
+
+}
+
 
 suspend fun main() {
 
-    val client : LLMClient = GoogleLLMClient(apiKey = System.getenv("GOOGLE_API_KEY"))
-    val model: LLModel = GoogleModels.Gemini2_5Flash
 
-    val prompt = prompt(
-        id = "translation-request",
-        params = LLMParams(temperature = 0.7)
-    ){
-        system("""
-       You're a banking assistant. You can send money by writing the following JSON:
-       {
-           "name": "send_money",
-           "params": {
-               "recipient": <recipient_name>,
-               "amount": <amount_in_euros>,
-               "purpose": <purpose_of_the_transaction>
-           }
-       }
-   """.trimIndent())
-        user(content = "Send 20 euros to Daniel for dinner at the restaurant")
+//    val client : LLMClient = GoogleLLMClient(apiKey = System.getenv("GOOGLE_API_KEY"))
+//    val model: LLModel = GoogleModels.Gemini2_5Pro
+
+
+    val executor = simpleOllamaAIExecutor(baseUrl = "http://localhost:11434/")
+    val model : LLModel = OllamaModels.Alibaba.QWEN_2_5_05B
+
+    val toolRegistry = ToolRegistry {
+        tool(::sendMoney)
     }
+    val agent = AIAgent(
+        executor = executor,
+        llmModel = model,
+        toolRegistry = toolRegistry,
+        systemPrompt = "You're a banking assistant. Accompany the user with their request."
+    )
 
-    val response = client.execute(prompt,model)
+    val userMessage = "Send 25 pesos to Daniel for dinner at the restaurant"
 
-    with(response.first()){
-        println("Translation: $content")
-        println("Meta-info: $metaInfo")
-    }
+    val response = agent.run(userMessage)
+
+    println("Final Result: $response")
 
 }
 
