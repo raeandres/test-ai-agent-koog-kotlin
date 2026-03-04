c6009c086f8a4902fe46a163c4bf71824c814cf7
Rae-An Andres <raeandres@users.noreply.github.com>
2026-03-04 21:12:23 +0800

    Refactored main file to remove API key
    Added the key in .env file
    added .env to gitignore

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,15 +10,25 @@ import ai.koog.prompt.params.LLMParams
 
 suspend fun main() {
 
-    val client : LLMClient = GoogleLLMClient(apiKey = "AIzaSyCY_dOBIdobrEtW3wxmyyGhjriP52BwomU")
+    val client : LLMClient = GoogleLLMClient(apiKey = System.getenv("GOOGLE_API_KEY"))
     val model: LLModel = GoogleModels.Gemini2_5Flash
 
-    val userMessage = readln()
     val prompt = prompt(
         id = "translation-request",
         params = LLMParams(temperature = 0.7)
     ){
-        user(content = "Translate this sentence to Tagalog/Filipino: $userMessage")
+        system("""
+       You're a banking assistant. You can send money by writing the following JSON:
+       {
+           "name": "send_money",
+           "params": {
+               "recipient": <recipient_name>,
+               "amount": <amount_in_euros>,
+               "purpose": <purpose_of_the_transaction>
+           }
+       }
+   """.trimIndent())
+        user(content = "Send 20 euros to Daniel for dinner at the restaurant")
     }
 
     val response = client.execute(prompt,model)
